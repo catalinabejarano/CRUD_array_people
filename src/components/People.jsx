956d2bbfeb0f4c0d0a9a121c6e89c20af330f8d3
Catalina Bejarano <catalina.bejarano@gmail.com>
2024-10-03 21:13:20 -0500

    Configuracion  deL metodo de Edit de las CARDS

--- src/components/People.jsx
+++ src/components/People.jsx
@@ -1,77 +1,103 @@
 import PropTypes from "prop-types";
 import { Person } from "./Person";
+import { useState } from "react";
 
-export const People = ({ people, setPeople }) => {
-  //Estado para gestionar el Id de la Persona
+export const People = ( { people, setPeople } ) => {
+
+  // Estado para gestionar el Id de la persona que se está editando
   const [editingId, setEditingId] = useState(null);
 
-  ///Estado para alamcenar temporalmente los datos de la persona que se esta editando
-  const [editedPerson, setEditedPerson] = useState({
-    name: "",
-    role: "",
-    img: "",
-  });
+  // Estado para establecer si se está editando a una persona
+  const [isEditing, setIsEditing] = useState(false);
 
-  //Metodo para  gestionar los campos del formulario
+  // Estado para almacenar temporalmente los datos de la persona que se está editando
+  const [editedPerson, setEditedPerson] = useState(
+    {
+      name: '',
+      role: '',
+      img: ''
+    }
+  );
+
+  // Método para gestionar los campos del formulario 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setEditedPerson((prevState) => ({
+    setEditedPerson(prevState => ({
       ...prevState,
-      [name]: value,
+      [name]: value
     }));
   };
 
-  //Metodo para crear nueva persona en el IT Team
+  // Método para crear una nueva persona en el Team
   const handleCreate = (e) => {
     e.preventDefault();
 
-    ///Agregar una persona al array
+    // Agregar una persona al array
     setPeople([...people, { id: people.length + 1, ...editedPerson }]);
 
-    //Reiniciar el estado del formulario
-    setEditedPerson({ name: "", role: "", img: "" });
+    // Reiniciar el estado del formulario 
+    setEditedPerson({ name: '', role: '', img: ''});
   };
+
+  // Método para editar a una persona
+  const handleEdit = (id) => {
+
+    setEditingId(id);
+    setIsEditing(true);
+    const personToEdit = people.find(person => person.id === id);
+
+    setEditedPerson({ ...personToEdit });
+  };
+
 
   return (
     <div>
-      <h2 className="text-center my-4">IT Team</h2>
-      <div className="container">
-        <div className="row d-flex flex-wrap row-cols-1 row-cols-md-2 row-cols-lg-3">
-          {people.map((people) => {
-            return (
-              <div key={people.id}>
-                <Person
-                  id={people.id}
-                  name={people.name}
-                  img={people.img}
-                  role={people.role}
-                />
-              </div>
-            );
-          })}
+      <h2 className='text-center my-4'>IT Team</h2>
+      <div className='container'>
+        <div className='row d-flex flex-wrap row-cols-1 row-cols-md-2 row-cols-lg-3'>
+          {
+            people.map((people) => {
+              return (
+                <div key={people.id}>
+                  <Person
+                    id={people.id}
+                    name={people.name}
+                    img={people.img}
+                    role={people.role}
+                    handleEdit={() => handleEdit(people.id)}
+                  />
+                </div>
+              );
+            })
+          }
         </div>
       </div>
-      {/*Formulario*/}
-      <div className="container">
-        <h2 className=" text-center mt-4">Crear Nuevo Empleado</h2>
-        <form action="">
+      {/* Formulario */}
+      <div className='container'>
+        <h2 className='text-center mt-4' >Crear Nuevo Empleado</h2>
+        <form>
           <div>
             <label htmlFor="name">Nombres</label>
-            <input
-              type="text"
-              name="name"
-              value={editedPerson.name}
-              required
-              className="form-control"
-            ></input>
+            <input type="text" name="name" value={editedPerson.name} onChange={handleChange} required className="form-control" />
+          </div>
+          <div>
+            <label htmlFor="role">Rol</label>
+            <input type="text" name="role" value={editedPerson.role} onChange={handleChange}  required className="form-control" />
+          </div>
+          <div>
+            <label htmlFor="img">Avatar</label>
+            <input type="text" name="img" value={editedPerson.img} onChange={handleChange}  required className="form-control" />
+          </div>
+          <div className="mt-2 text-center">
+            <button type="submit" className="btn btn-primary">Modificar</button>
           </div>
         </form>
       </div>
     </div>
-  );
+  )
 };
 
 People.propTypes = {
   people: PropTypes.array,
-  setPeople: PropTypes.func,
-};
+  setPeople: PropTypes.func
+}
--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,33 +1,28 @@
+
 import PropTypes from "prop-types";
 
-
-export const Person = ({id, name, role, img}) => {
+export const Person = ({ name, role, img, handleEdit }) => {
   return (
-    <div className="col">
-      <div className="card" style={{ width: "18rem;" }}>
-        <img
-          src={img}
-          className="card-img-top"
-          alt="avatar1"
-        />
-        <div className="card-body">
-          <h5 className="card-title">{name}</h5>
-          <p className="card-text">{role}</p>
-        </div>
-        <div className="container mb-4">
-          <button className="btn btn-success mx-2">Editar</button>
-          <button className="btn btn-danger">Eliminar</button>
-        </div>
+    <div className='col'>
+      <div className="card" style={{width: "18rem"}}>
+        <img src={img} className="card-img-top" alt={name} />
+          <div className="card-body">
+            <h3 className="card-title">{name}</h3>
+            <p className="card-text">{role}</p>
+          </div>
+          <div className='container mb-4'>
+            <button className="btn btn-success mx-2" onClick={handleEdit}>Editar</button>
+            <button className="btn btn-danger">Eliminar</button>
+          </div>
       </div>
     </div>
-  );
+  )
 };
 
 Person.propTypes = {
-
-    id: PropTypes.number,
-    name: PropTypes.string,
-    role: PropTypes.string,
-    img: PropTypes.string,
-        
+  id: PropTypes.number,
+  name: PropTypes.string,
+  role: PropTypes.string,
+  img: PropTypes.string,
+  handleEdit: PropTypes.func
 }
